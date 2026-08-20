ab0396903c4a3a0f208f2788bf6a14ab407704a7
luciengauthier1960-ux <lucien.gauthier.1960@gmail.com>
2026-08-20 11:48:52 +0200

    Update AlarmReceiver.kt

--- app/src/main/java/com/example/minuteur/AlarmReceiver.kt
+++ app/src/main/java/com/example/minuteur/AlarmReceiver.kt
@@ -6,6 +6,8 @@ import android.content.Intent
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        AlarmSound.start(context)
+
         val alarmIntent = Intent(context, AlarmActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
         }
